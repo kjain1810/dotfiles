17594d69f68b8f8107ea09b843ec0d402f38705b
kjain1810 <kunaljain.2000@yahoo.in>
2022-05-31 04:21:40 +0530

    Update

---
+++ polybar/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+pkill polybar
+
+# polybar --reload example &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+
